53cfa39c150eeb8251e39c96ae098ec0f0caa51b
Ganesh K <ganesh3597@gmail.com>
2018-07-27 23:28:23 +0530

    Test Addition

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,12 +3,33 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("Testing available mathematical operations", "[intal]") {
-	Intal num1("123456789");
-	Intal num2("987654321");
+TEST_CASE("Testing addition operations", "[add]") {
 	
-	SECTION("Testing if addition works") {
+	SECTION("Testing if simple addition works") {
+		Intal num1("123456789");
+		Intal num2("987654321");
 		Intal result("1111111110");
+		REQUIRE(((num1+num2) == result));
+	}
+	
+	SECTION("Testing very large test-cases") {
+		Intal num1("99999999999999999999999999999999999999999999999999999");
+		Intal num2("99999999999999999999999999999999999999999999999999999");
+		Intal result("199999999999999999999999999999999999999999999999999998");
+		REQUIRE(((num1+num2) == result));
+	}
+	
+	SECTION("Testing positive-negative test-cases") {
+		Intal num1("99999999999999999999999999999999999999999999999999999");
+		Intal num2("-99999999999999999999999999999999999999999999999999999");
+		Intal result("0");
+		REQUIRE(((num1+num2) == result));
+	}
+	
+	SECTION("Testing negative-negative test-cases") {
+		Intal num1("-99999999999999999999999999999999999999999999999999999");
+		Intal num2("-99999999999999999999999999999999999999999999999999999");
+		Intal result("-199999999999999999999999999999999999999999999999999998");
 		REQUIRE(((num1+num2) == result));
 	}
 	
