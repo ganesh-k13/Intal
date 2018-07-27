16b02636d7e2f11eb71c83d805e163376ca1fa38
Ganesh K <ganesh3597@gmail.com>
2018-07-27 23:34:21 +0530

    Test Subtraction

--- tests/test.cpp
+++ tests/test.cpp
@@ -34,3 +34,42 @@ TEST_CASE("Testing addition operations", "[add]") {
 	}
 	
 }
+
+TEST_CASE("Testing subtraction operations", "[sub]") {
+	
+	SECTION("Testing if simple subtraction works") {
+		Intal num1("123456789");
+		Intal num2("987654321");
+		Intal result("-864197532");
+		REQUIRE(((num1-num2) == result));
+	}
+	
+	SECTION("Testing very large test-cases") {
+		Intal num1("99999999999999999999999999999999999999999999999999999");
+		Intal num2("99999999999999999999999999999999999999999999999999999");
+		Intal result("0");
+		REQUIRE(((num1-num2) == result));
+	}
+	
+	SECTION("Testing positive-negative test-cases") {
+		Intal num1("99999999999999999999999999999999999999999999999999999");
+		Intal num2("-99999999999999999999999999999999999999999999999999999");
+		Intal result("199999999999999999999999999999999999999999999999999998");
+		REQUIRE(((num1-num2) == result));
+	}
+	
+	SECTION("Testing negative-negative test-cases") {
+		Intal num1("-99999999999999999999999999999999999999999999999999999");
+		Intal num2("-99999999999999999999999999999999999999999999999999999");
+		Intal result("0");
+		REQUIRE(((num1-num2) == result));
+	}
+	
+	SECTION("Testing negative-positive test-cases") {
+		Intal num1("-99999999999999999999999999999999999999999999999999999");
+		Intal num2("99999999999999999999999999999999999999999999999999999");
+		Intal result("-199999999999999999999999999999999999999999999999999998");
+		REQUIRE(((num1-num2) == result));
+	}
+	
+}
